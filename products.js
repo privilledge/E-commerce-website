0602783de5ca192f304b01fe8876d4384355bbdc
privilledge <privieroyalty@gmail.com>
2023-01-18 15:01:05 +0200

    initial commit

---
+++ products.js
@@ -0,0 +1,77 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+    123: {
+        name: "CP6 combi plug",
+        desc: "For reparing bike punctures",
+        img: "plug (1).jpg",
+        price: 5.25
+
+    },
+    124: {
+        name: "CP9 combi plug",
+        desc: "For reparing bike punctures",
+        img: "plug (2).jpg",
+        price: 4.83
+    },
+    125: {
+        name: "CP7 combi plug",
+        desc: "For reparing bike punctures",
+        img: "plug (5).jpg",
+        price: 5.12
+    },
+    126: {
+        name: "CP8 combi plug",
+        desc: "For reparing bike punctures ",
+        img: "plug (4).jpg",
+        price: 4.80
+    },
+
+    127: {
+        name: "R6 tube patch",
+        desc: "For repairing inner tubes.",
+        img: "patches (6).jpg",
+        price: 3.00
+    },
+    128: {
+        name: " L8 tube patch",
+        desc: "For repairing inner tubes.",
+        img: "patches (7).jpg",
+        price: 6.00
+    },
+    129: {
+        name: "R4 tube patch",
+        desc: "For repairing inner tubes.",
+        img: "patches (8).jpg",
+        price: 4.23
+    },
+    130: {
+        name: "Ply patches",
+        desc: "For repairing inner tubes.",
+        img: "patches (11).jpg",
+        price: 6.45
+    },
+    131: {
+        name: "NO.5 Cross ply",
+        desc: "Chemical Vulcanising Systems.",
+        img: "cross-ply (1).jpg",
+        price: 15.25
+    },
+    132: {
+        name: "NO.6 Cross ply",
+        desc: "Chemical Vulcanising Systems.",
+        img: "cross-ply (3).jpg",
+        price: 15.25
+    },
+    133: {
+        name: "NO.7 Cross ply",
+        desc: "Chemical Vulcanising Systems.",
+        img: "cross-ply (2).jpg",
+        price: 15.25
+    },
+    134: {
+        name: "Sealant glue",
+        desc: "Tire repair glue 30ml. ",
+        img: "glue (1).jpg",
+        price: 2.05
+    }
+};
